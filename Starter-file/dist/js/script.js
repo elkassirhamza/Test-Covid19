4b31df81f2fe052bed8330b9125d65b5aa1dc18d
elkassir hamza <elkassir.hamza05@gmail.com>
2020-04-19 16:06:06 +0000

    page test (premier pas)

---
+++ Starter-file/dist/js/script.js
@@ -0,0 +1,12 @@
+let btn_1 = document.querySelector("#btn_1");
+let preamble = document.querySelector("#preambule");
+let quest = document.querySelector("#question_1");
+
+
+btn_1.addEventListener("click", function () {
+    preamble.style.display = "none";
+    quest.style.display = "block";
+
+
+
+});
