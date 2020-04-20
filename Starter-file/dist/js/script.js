a958be2a846abc937af7f3db388756647c9c9d2d
elkassir hamza <elkassir.hamza05@gmail.com>
2020-04-20 23:05:51 +0000

    navigation entre l deux premier page test

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -1,12 +1,28 @@
 let btn_1 = document.querySelector("#btn_1");
 let preamble = document.querySelector("#preambule");
 let quest = document.querySelector("#question_1");
-
+let goldpoint = document.querySelector(".active");
 
 btn_1.addEventListener("click", function () {
     preamble.style.display = "none";
     quest.style.display = "block";
-
+    goldpoint.style.animation = "mymove .5s forwards";
 
 
 });
+let btn_2 = document.querySelector("#Q2");
+let quest_2 = document.querySelector("#question_2");
+
+
+
+back = document.querySelector("#retur");
+
+btn_2.addEventListener("click", function () {
+    quest.style.display = "none";
+    quest_2.style.display = "block";
+
+    back.addEventListener("click", function () {
+        quest.style.display = "block";
+        quest_2.style.display = "none";
+    })
+})
