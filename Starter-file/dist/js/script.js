e4fae458c6305e0f0be9cb14b5e8c464e127c857
elkassir hamza <elkassir.hamza05@gmail.com>
2020-05-07 04:45:59 +0000

    add some JS

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -1,23 +1,30 @@
 const questionsList = [{
         text: 'Pensez-vous avoir ou avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?',
-        choices: `<label for="oui">Oui</label>
-                    <input type="radio" value="oui" id="oui" name="choice"><br>
-                    <label for="non">Non</label>
-                    <input type="radio" id="non" value="non" name="choice"><br>`,
+        choices: `<div class="radio_1">
+                <div class="option">  
+                <input type="radio" value="oui" id="oui" name="choice">
+                    <label for="oui">Oui</label>
+                </div >
+                <br>
+                <div class="option">
+                    <input type="radio" id="non" value="non" name="choice">
+                     <label for="non">Non</label> 
+                </div>
+         </div>`,
         number: 1,
         type: 1,
 
     },
     {
-        text: 'Ces dernières 48 heures, quelle a été votre température la plus élevée ?',
-        choices: `<label for="numerique">degrée</label>
-                    <input type="text"  id="numerique" >`,
+        text: 'quelle est votre température ?',
+        choices: `<input type="number" placeholder="37" id="numerique" >
+        <label for="numerique">degrée</label>`,
         number: 2,
         type: 2,
 
     },
     {
-        text: 'Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?',
+        text: 'Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?',
         choices: `<label for="oui">Oui</label>
                     <input type="radio" value="oui" id="oui" name="choice"><br>
                     <label for="non">Non</label>
@@ -26,7 +33,7 @@ const questionsList = [{
         type: 1,
     },
     {
-        text: 'Ces derniers jours, avez-vous noté une forte diminution ou perte de votre goût ou de votre odorat ?',
+        text: 'Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?',
         choices: `<label for="oui">Oui</label>
                     <input type="radio" value="oui" id="oui" name="choice"><br>
                     <label for="non">Non</label>
@@ -35,7 +42,7 @@ const questionsList = [{
         type: 1,
     },
     {
-        text: 'Ces derniers jours, avez-vous eu un mal de gorge et/ou des douleurs musculaires et/ou des courbatures inhabituelles ?',
+        text: 'Avez-vous un mal de gorge apparu ces derniers jours ?',
         choices: `<label for="oui">Oui</label>
                     <input type="radio" value="oui" id="oui" name="choice"><br>
                     <label for="non">Non</label>
@@ -44,7 +51,7 @@ const questionsList = [{
         type: 1,
     },
     {
-        text: 'Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.',
+        text: 'Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?',
         choices: `<label for="oui">Oui</label>
                     <input type="radio" value="oui" id="oui" name="choice"><br>
                     <label for="non">Non</label>
@@ -53,7 +60,7 @@ const questionsList = [{
         type: 1,
     },
     {
-        text: 'Ces derniers jours, avez-vous une fatigue inhabituelle ?',
+        text: 'Avez-vous une fatigue inhabituelle ces derniers jours ?',
         choices: `<label for="oui">Oui</label>
                     <input type="radio" value="oui" id="oui" name="choice"><br>
                     <label for="non">Non</label>
@@ -71,7 +78,7 @@ const questionsList = [{
         type: 1,
     },
     {
-        text: "Depuis 24 heures ou plus, êtes-vous dans l'impossibilité de vous alimenter ou de boire ?",
+        text: "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?",
         choices: `<label for="oui">Oui</label>
                     <input type="radio" value="oui" id="oui" name="choice"><br>
                     <label for="non">Non</label>
@@ -80,7 +87,7 @@ const questionsList = [{
         type: 1,
     },
     {
-        text: 'Ces dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?',
+        text: 'Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?',
         choices: `<label for="oui">Oui</label>
                     <input type="radio" value="oui" id="oui" name="choice"><br>
                     <label for="non">Non</label>
@@ -89,138 +96,161 @@ const questionsList = [{
         type: 1,
     },
     {
+        text: 'Comment vous sentez-vous ? ',
+        choices: `<div class="fourchoices">
+		<div class="choices">
+		<label for="fatigue">Fatigué</label>
+		<input type="radio" value="fatigué" id="fatigue" name="choice"><br>
+		</div>
+		<div class="choices">
+		<label for="bien">Bien</label>
+		<input type="radio" id="bien" value="bien" name="choice"><br>
+		</div>
+	</div>
+	<div class="fourchoices">
+		<div class="choices">
+		<label for="trop-fatigue">Trop fatigué</label>
+		<input type="radio" value="trop-fatigué" id="trop-fatigue" name="choice"><br>
+	</div>
+		<div class="choices">
+		<label for="moyen">Moyen</label>
+		<input type="radio" id="moyen" value="moyen" name="choice"><br>
+	</div>
+	</div>`,
+        number: 11,
+        type: 1,
+    },
+    {
         text: `Quel est votre âge ? 
         Ceci, afin de calculer un facteur de risque spécifique.`,
-        choices: `<label for="numerique">ans</label>
-        <input type="text" id="numerique" name="choice">`,
-        number: 11,
+        choices: `<input type="text" id="numerique" name="choice">
+        <label for="numerique">ans</label>`,
+        number: 12,
         type: 2,
     },
     {
         text: `Quel est votre taille ? 
-Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.`,
-        choices: `<label for="numerique">cm</label>
-<input type="text" id="numerique" name="choice">`,
-        number: 12,
+        Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.`,
+        choices: `<input type="text" id="numerique" name="choice">
+        <label for="numerique">cm</label>`,
+        number: 13,
         type: 2,
     },
     {
         text: `Quel est votre poids ?
 Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.`,
-        choices: `<label for="numerique">kg</label>
-<input type="text" id="numerique" name="choice">`,
-        number: 13,
+        choices: `<input type="text" id="numerique" name="choice">
+        <label for="numerique">kg</label>`,
+        number: 14,
         type: 2
     },
     {
-        text: `Avez-vous de l’hypertension artérielle mal équilibrée ?
-Ou avez-vous une maladie cardiaque ou vasculaire ?
-Ou prenez vous un traitement à visée cardiologique ?`,
+        text: `Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>
-<label for="sspas">Ne sait pas</label>
-<input type="radio" id="sspas" value="sspas" name="choice" >`,
-        number: 14,
+                <input type="radio" value="oui" id="oui" name="choice"><br>
+                <label for="non">Non</label>
+                <input type="radio" id="non" value="non" name="choice"><br>
+                <label for="sspas">Ne sait pas</label>
+                <input type="radio" id="sspas" value="sspas" name="choice" >`,
+        number: 15,
         type: 1
     },
     {
         text: `Êtes-vous diabétique ?`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>`,
-        number: 15,
-        type: 1
-    },
-    {
-        text: `Avez-vous ou avez-vous eu un cancer ces trois dernières années ?`,
-        choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>`,
+                <input type="radio" value="oui" id="oui" name="choice"><br>
+                <label for="non">Non</label>
+                <input type="radio" id="non" value="non" name="choice"><br>`,
         number: 16,
         type: 1
     },
     {
-        text: `Avez-vous une maladie respiratoire ?
-Ou êtes-vous suivi par un pneumologue ?`,
+        text: `Avez-vous ou avez-vous eu un cancer ?`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>`,
+                <input type="radio" value="oui" id="oui" name="choice"><br>
+                <label for="non">Non</label>
+                <input type="radio" id="non" value="non" name="choice"><br>`,
         number: 17,
+        type: 1
+    },
+    {
+        text: `Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ? ?
+        Ou êtes-vous suivi par un pneumologue ?`,
+        choices: `<label for="oui">Oui</label>
+            <input type="radio" value="oui" id="oui" name="choice"><br>
+            <label for="non">Non</label>
+            <input type="radio" id="non" value="non" name="choice"><br>`,
+        number: 18,
         type: 1
     },
     {
         text: `Avez-vous une insuffisance rénale chronique dialysée ?`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>`,
-        number: 18,
+                <input type="radio" value="oui" id="oui" name="choice"><br>
+                <label for="non">Non</label>
+                <input type="radio" id="non" value="non" name="choice"><br>`,
+        number: 19,
         type: 1
     },
     {
         text: `Avez-vous une maladie chronique du foie ?`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>`,
-        number: 19,
+                <input type="radio" value="oui" id="oui" name="choice"><br>
+                <label for="non">Non</label>
+                <input type="radio" id="non" value="non" name="choice"><br>`,
+        number: 20,
         type: 1
     },
     {
         text: `Êtes-vous enceinte ?`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>
-<label for="sspas">Non applicable</label>
-<input type="radio" id="sspas" value="sspas" name="choice" >`,
-        number: 20,
+            <input type="radio" value="oui" id="oui" name="choice"><br>
+            <label for="non">Non</label>
+            <input type="radio" id="non" value="non" name="choice"><br>
+            <label for="sspas">Non applicable</label>
+            <input type="radio" id="sspas" value="sspas" name="choice" >`,
+        number: 21,
         type: 1
     },
     {
         text: `Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>
-<label for="sspas">Ne sait pas</label>
-<input type="radio" id="sspas" value="sspas" name="choice" >`,
-        number: 21,
+                <input type="radio" value="oui" id="oui" name="choice"><br>
+                <label for="non">Non</label>
+                <input type="radio" id="non" value="non" name="choice"><br>
+                <label for="sspas">Ne sait pas</label>
+                <input type="radio" id="sspas" value="sspas" name="choice" >`,
+        number: 22,
         type: 1
     },
     {
         text: `Prenez-vous un traitement immunosuppresseur ?
-C’est un traitement qui diminue vos défenses contre les infections.
-Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).`,
+        C’est un traitement qui diminue vos défenses contre les infections.
+        Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).`,
         choices: `<label for="oui">Oui</label>
-<input type="radio" value="oui" id="oui" name="choice"><br>
-<label for="non">Non</label>
-<input type="radio" id="non" value="non" name="choice"><br>
-<label for="sspas">Ne sait pas</label>
-<input type="radio" id="sspas" value="sspas" name="choice">`,
-        number: 22,
+                <input type="radio" value="oui" id="oui" name="choice"><br>
+                <label for="non">Non</label>
+                <input type="radio" id="non" value="non" name="choice"><br>
+                <label for="sspas">Ne sait pas</label>
+                <input type="radio" id="sspas" value="sspas" name="choice">`,
+        number: 23,
         type: 1
     }
 ];
 
 
-let lancer = document.querySelector("#btn_1");
-let preamble = document.querySelector("#preambule");
-let quest = document.querySelector("#question_1");
-let quest1 = document.querySelector(".question_1");
+const lancer = document.querySelector("#btn_1");
+const preamble = document.querySelector("#preambule");
+const quest = document.querySelector("#question_1");
+const quest1 = document.querySelector(".question_1");
 // let goldpoint = document.querySelector(".active");
-let pointQuestionnaire = document.querySelector(".questionnaire");
-let pointInfo = document.querySelector(".informations");
+const pointQuestionnaire = document.querySelector(".questionnaire");
+const pointInfo = document.querySelector(".informations");
 const questionText = document.querySelector('.questions__text');
 const suivant = document.querySelector('.btn_next');
 const precedente = document.querySelector('.btn-return');
 const form = document.querySelector('.questions__form');
-let conseil = document.querySelector('.consei');
+const conseil = document.querySelector('.consei');
 
 const progressBar = document.querySelector('.progressBar');
 
@@ -264,20 +294,28 @@ function changeTextQuest(compteur) {
 suivant.addEventListener('click', () => {
     conseil.style.display = "none";
     let details = document.querySelector('#numerique');
-    if (form.children[1].id === 'numerique') {
+    if (form.children[0].id === 'numerique') {
 
         if (details.value === "") {
             alert("Enter valid number")
             return;
-        } else if (details.value < 36 || details.value > 41) {
-            alert('Entrer votre temperature');
+        } else if (counter == 2 && (details.value < 36 || details.value > 41)) {
+            alert('Entrer your temperature');
+            return;
+        } else if (counter === 12 && (details.value > 110)) {
+            alert("l'age doit etre inferieure a 110");
+            return;
+        } else if (counter === 13 && (details.value < 100 || details.value > 200)) {
+            alert('Enter taille entre 100 et 200');
+            return;
+        } else if (counter === 14 && (details.value < 40 || details.value > 200)) {
+            alert('Poids etre 40 et 200');
             return;
         } else {
             arr2.push(details.value);
             console.log(details.value);
             counter += 1;
         }
-
     } else if (questionsList[counter - 1].type === 1) {
         var valeur = document.querySelector('input[name=choice]:checked').value;
         if (valeur === 'non' && counter === 1) {
@@ -308,13 +346,13 @@ precedente.addEventListener('click', () => {
     if (counter <= 1) {
         precedente.style.display = "none";
     }
-    
+
     if (counter === 3 && arr2[0] === 'non') {
         counter -= 1;
         arr2.pop();
         precedente.style.display = "none";
     }
-   
+
     counter -= 1;
 
     arr2.pop();
@@ -326,8 +364,8 @@ const progression = document.querySelector('.progress')
 const questNumber = document.querySelector('.prognum')
 
 function proBar(pourcent) {
-    progression.style.width = `${ 100 / 22 * pourcent}%`;
-    questNumber.textContent = `${pourcent}/22`
+    progression.style.width = `${ 100 / 23 * pourcent}%`;
+    questNumber.textContent = `${pourcent}/23`
 }
 
 
